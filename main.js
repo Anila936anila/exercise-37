88f29e47575fde4c75e9629d2820627af948e499
Anila936anila <161087832+Anila936anila@users.noreply.github.com>
2024-02-27 13:16:43 -0800

    Large T Shirt
    Typescript 45Q assignment exercise 37

---
+++ main.js
@@ -0,0 +1,8 @@
+"use strict";
+function make_shirt(size = "Large", messege = "I love my Mom") {
+    return console.log(`Size: ${size}, Message: '${messege}'`);
+}
+// Custome shirt with a different message ans size
+make_shirt("Small", "Green Pakistan Clean Pakistan");
+make_shirt("Medium", "Safe Your Environment");
+make_shirt("Large", "Safe Water for future");
